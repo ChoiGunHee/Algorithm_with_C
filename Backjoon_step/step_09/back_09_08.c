2430bfc0392eb4adbed1271bb6660af4388ee565
GunHee Choi <godrjsmgl@gmail.com>
2021-04-10 11:20:01 +0000

    Solve Backjoon Step09 09_08

--- Backjoon_step/step_09/back_09_08.c
+++ Backjoon_step/step_09/back_09_08.c
@@ -21,6 +21,34 @@
 #include <stdio.h>
 
 int main(void) {
-
+	int x[4];
+	int y[4];
+	int i;
+	
+	for(i=0; i<3; i++)
+		scanf("%d %d", &x[i], &y[i]);
+	
+	if(x[0] == x[1]) {
+		x[3] = x[2];
+	} else {
+		if(x[0] == x[2]) {
+			x[3] = x[1];
+		} else {
+			x[3] = x[0];
+		}
+	}
+	
+	if(y[0] == y[1]) {
+		y[3] = y[2];
+	} else {
+		if(y[0] == y[2]) {
+			y[3] = y[1];
+		} else {
+			y[3] = y[0];
+		}
+	}
+	
+	printf("%d %d\n", x[3], y[3]);
+	
 	return 0;	
 }
