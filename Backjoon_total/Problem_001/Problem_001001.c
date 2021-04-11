86e474e55d3e7269375f8bfd04c284ab1d00a330
GunHee Choi <godrjsmgl@gmail.com>
2021-04-11 14:25:22 +0000

    Add and Solve Backjoon total problem 1000, 1001

---
+++ Backjoon_total/Problem_001/Problem_001001.c
@@ -0,0 +1,24 @@
+/**
+* 2021. 02. 05
+* Creater : Gunhee Choi
+* Problem Number : 1001
+* Title : A-B
+
+* Problem :
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+* Input : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+	3 2
+
+* Output : 1
+
+**/
+
+#include <stdio.h>
+
+int main() {
+	int a,b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a-b);
+	return 0;
+}
