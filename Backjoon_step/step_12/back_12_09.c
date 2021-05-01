05a2c48accc5bf7121d30afd4c1dc5bbf261a92d
GunHee Choi <godrjsmgl@gmail.com>
2021-05-01 13:41:41 +0000

    Solve Backjoon Step12 12_09 with C

---
+++ Backjoon_step/step_12/back_12_09.c
@@ -0,0 +1,70 @@
+/**
+* 2021. 05. 01
+* Creater : Gunhee Choi
+* Problem Number : 10814
+* Title : 나이순 정렬
+
+* Problem :
+온라인 저지에 가입한 사람들의 나이와 이름이 가입한 순서대로 주어진다. 이때, 회원들을 나이가 증가하는 순으로, 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬하는 프로그램을 작성하시오.
+
+* Input :
+첫째 줄에 온라인 저지 회원의 수 N이 주어진다. (1 ≤ N ≤ 100,000)
+
+둘째 줄부터 N개의 줄에는 각 회원의 나이와 이름이 공백으로 구분되어 주어진다. 나이는 1보다 크거나 같으며, 200보다 작거나 같은 정수이고, 이름은 알파벳 대소문자로 이루어져 있고, 길이가 100보다 작거나 같은 문자열이다. 입력은 가입한 순서로 주어진다.
+
+	3
+	21 Junkyu
+	21 Dohyun
+	20 Sunyoung
+
+* Output :
+첫째 줄부터 총 N개의 줄에 걸쳐 온라인 저지 회원을 나이 순, 나이가 같으면 가입한 순으로 한 줄에 한 명씩 나이와 이름을 공백으로 구분해 출력한다.
+
+	20 Sunyoung
+	21 Junkyu
+	21 Dohyun
+
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_ARR 100000
+#define MAX_STR 101
+
+typedef struct person {
+	char name[MAX_STR];
+	int age;
+} Person;
+
+//10만개가 넘가면 main의 스택할당으로는 감당안됨.
+struct person list[MAX_ARR];
+
+int compare(const void * a, const void * b)
+{
+    struct person * person1 = (struct person *) a;
+    struct person * person2 = (struct person *) b;
+
+	if( person1->age > person2->age ) return 1;
+	else if( person1->age < person2->age ) return -1;
+	else return 0;
+	
+    return 0;
+}
+
+int main(void) {
+	int N;
+	int i;
+	
+	scanf("%d", &N);
+	for(i=0; i<N; i++) {
+		scanf("%d %s", &list[i].age, list[i].name);
+	}
+	
+	qsort(list, N, sizeof(Person), compare);
+	
+	for(i=0; i<N; i++)
+		printf("%d %s\n", list[i].age, list[i].name);
+	
+	return 0;	
+}
