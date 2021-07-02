b0ded2c4262cf2165ab4a301929fbf3df43719c0
GunHee Choi <godrjsmgl@gmail.com>
2021-07-02 23:19:19 +0000

    Study Bublem Sort with C

--- Datastructure/Sort/Bubble_sort.c
+++ Datastructure/Sort/Bubble_sort.c
@@ -1,4 +1,43 @@
-/* 2021. 06. 25
+/* 2021. 06. 26
  * Creater : Gunhee Choi
  * This file is Bublle Sort Simulation.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 10
+#define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t))
+
+int list[MAX_SIZE];
+int n;
+
+void buble_sort(int list[], int n) {
+	int i, j, temp;
+	
+	for(i=n-1; i>0; i--)
+		for(j=0; j<i; j++)
+			if(list[j] > list[j+1])
+				SWAP(list[j], list[j+1], temp);
+}
+
+int main(void) {
+	int i;
+	n = MAX_SIZE;
+	srand(time(NULL));
+	
+	for(i=0; i<n; i++) {
+		list[i] = rand() % 100;
+		printf("%d ", list[i]);
+	}
+	printf("\n");
+	
+	buble_sort(list, n);
+	
+	for(i=0; i<n; i++)
+		printf("%d ", list[i]);
+	printf("\n");
+	
+	return 0;
+}
