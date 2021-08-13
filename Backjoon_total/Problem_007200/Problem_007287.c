7e3937129b647516b008d5aea44aa0dbc8f27920
GunHee Choi <godrjsmgl@gmail.com>
2021-08-13 14:45:58 +0000

    Solve Backjoon Total Problem 7287 with C

---
+++ Backjoon_total/Problem_007200/Problem_007287.c
@@ -0,0 +1,27 @@
+/**
+* 2021. 08. 13
+* Creater : Gunhee Choi
+* Problem Number : 7287
+* Title : 집합
+
+* Problem :
+자신이 백준 온라인 저지(BOJ)에서 맞은 문제의 수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
+
+* Input :
+
+* Output :
+첫 줄에 자신이 맞은 문제의 수, 둘째 줄에 아이디를 출력한다.
+
+	123
+	Your_ICPC_Team_Name
+
+
+**/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("170\n");
+	printf("godrjsmgl\n");
+	return 0;	
+}
