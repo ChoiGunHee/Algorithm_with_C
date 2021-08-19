907f74445a385d36dcae75d38d4d8c009be7b830
GunHee Choi <godrjsmgl@gmail.com>
2021-08-19 14:46:26 +0000

    Add Compare func

--- Basic_SourceCode/C_macro_code.c
+++ Basic_SourceCode/C_macro_code.c
@@ -1,2 +1,14 @@
 #define max(a,b) (a)>=(b)?(a):(b)
 #define min(a,b) (a)<=(b)?(a):(b)
+
+int compare(const void *a, const void *b) {
+	int num1 = *(int *)a;
+	int num2 = *(int *)b;
+
+	if(num1 < num2)
+		return -1;
+	else if(num1 > num2)
+		return 1;
+
+	return 0;
+}
