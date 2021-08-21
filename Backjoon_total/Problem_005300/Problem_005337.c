07d435cb5f58361e97465496ddfdc208aa461b7f
GunHee Choi <godrjsmgl@gmail.com>
2021-08-21 02:12:16 +0000

    Solve Backjoon Total Problem 5337 with C

---
+++ Backjoon_total/Problem_005300/Problem_005337.c
@@ -0,0 +1,28 @@
+/**
+* 2021. 08. 21
+* Creater : Gunhee Choi
+* Problem Number : 5337
+* Title : 웰컴
+
+* Problem :
+Welcome을 예제 출력처럼 출력하는 프로그램을 작성하시오.
+
+* Input :
+
+* Output :
+Welcome을 아래 예제 출력처럼 출력한다.
+
+.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+	
+**/
+
+#include <stdio.h>
+
+int main(void) {
+	printf(".  .   .\n");
+	printf("|  | _ | _. _ ._ _  _\n");
+	printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+	return 0;	
+}
