bbbdfdd762d85d1e7e1954b179058542010f16e3
GunHee Choi <godrjsmgl@gmail.com>
2021-08-25 09:55:21 +0000

    Solve Backjon total Problem 11942 with C

---
+++ Backjoon_total/Problem_011900/Problem_011942.c
@@ -0,0 +1,25 @@
+/**
+* 2021. 08. 25
+* Creater : Gunhee Choi
+* Problem Number : 11942
+* Title : 고려대는 사랑입니다
+
+* Problem :
+고려대학교는 사랑입니다.
+
+* Input :
+	
+* Output :
+첫째 줄에 “고려대학교”를 출력하세요.
+
+	고려대학교
+
+**/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("고려대학교\n");
+	
+	return 0;	
+}
