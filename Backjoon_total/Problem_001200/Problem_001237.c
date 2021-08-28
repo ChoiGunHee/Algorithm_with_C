720ce30de71ae4285814956045cd6232108c63fd
GunHee Choi <godrjsmgl@gmail.com>
2021-08-28 07:05:59 +0000

    Solve Backjoon Total Problem 1237 with C

---
+++ Backjoon_total/Problem_001200/Problem_001237.c
@@ -0,0 +1,24 @@
+/**
+* 2021. 08. 28
+* Creater : Gunhee Choi
+* Problem Number : 1237
+* Title : 정ㅋ벅ㅋ
+
+* Problem :
+이 문제를 푸는 자 우주를 정ㅋ벅ㅋ한다.
+
+* Input :
+우주를 정ㅋ벅ㅋ하는 자에겐 입력 따위 필요 없다.
+	
+* Output :
+첫째 줄에 문제의 정답을 출력한다.
+
+**/
+
+#include <stdio.h>
+ 
+int main(void) {
+    printf("문제의 정답");
+ 
+    return 0;
+}
