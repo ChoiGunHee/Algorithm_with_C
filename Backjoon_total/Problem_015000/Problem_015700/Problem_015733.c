400de64db219dd398b0ce3f7acb73d4d7f8e33f4
GunHee Choi <godrjsmgl@gmail.com>
2021-09-02 11:09:57 +0000

    Solve backjoon Total Problem 15733 with C

---
+++ Backjoon_total/Problem_015000/Problem_015700/Problem_015733.c
@@ -0,0 +1,25 @@
+/**
+* 2021. 09. 02
+* Creater : Gunhee Choi
+* Problem Number : 15733
+* Title : 나는 누구인가
+
+* Problem :
+당신은 지금 코딩을 하고있다. 그럼 당신은 어떤 사람인가.
+
+* Input :
+없다.
+
+* Output :
+“I'm Sexy”를 출력한다.
+
+	I'm Sexy
+	
+**/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("I'm Sexy\n");
+	return 0;	
+}
