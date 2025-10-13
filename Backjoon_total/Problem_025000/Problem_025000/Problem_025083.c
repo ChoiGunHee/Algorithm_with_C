a86b07945fcdd01fff70ecdc71ee45249263b990
ChoiGunHee <godrjsmgl@gmail.com>
2025-10-14 02:15:11 +0900

    Solved Backjoon Probelm 25083

---
+++ Backjoon_total/Problem_025000/Problem_025000/Problem_025083.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("         ,r'\"7\n"
+           "r`-_   ,'  ,/\n"
+           " \\. \". L_r'\n"
+           "   `~\\/\n"
+           "      |\n"
+           "      |\n");
+    return 0;
+}
